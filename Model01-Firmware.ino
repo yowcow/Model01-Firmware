d27bf3ecedd99dc0d2928adce090a777ffa73c30
yowcow <yowcow@cpan.org>
2018-06-24 20:11:15 +0900

    numlock=rightgui

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -139,7 +139,8 @@ KEYMAPS(
    Key_LeftControl, Key_Spacebar, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   //M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_RightGui,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    //Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
